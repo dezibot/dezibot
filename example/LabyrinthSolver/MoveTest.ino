ba382e3e305b84ddc5f0195261f7fd82139798a3
Vladislav Antonov <vladislav.antonov@stud.htwk-leipzig.de>
2024-12-02 16:54:53 +0100

    TODOs added

--- example/LabyrinthSolver/MoveTest.ino
+++ example/LabyrinthSolver/MoveTest.ino
@@ -3,6 +3,7 @@
 Dezibot dezibot = Dezibot();
 
 const double BASE_SPEED = 3900;
+const double MAX_SPEED = 8192;
 
 void setup() {
   Serial.begin(115200);
@@ -15,22 +16,33 @@ void loop() {
   getColorPercentages(percentageRed, percentageGreen, percentageBlue);
 
   dezibot.display.clear();
-  dezibot.display.println("R: " + String(percentageRed, 2) + " G: " + String(percentageGreen, 2));
+  dezibot.display.println("R:" + String(percentageRed, 2) + " G:" + String(percentageGreen, 2));
   delay(100);
+
+  // while(true){
+  // dezibot.motion.right.setSpeed(8192);
+  // if(percentageGreen-percentageRed >= 1){
+
+  // }
+  // }
 
   if (percentageRed > percentageGreen) {
     displayMessage("ROT > GRÜN: Rechts drehen.");
     dezibot.motion.left.setSpeed(BASE_SPEED);
-    dezibot.motion.right.setSpeed(2500); // Nach rechts drehen
+    dezibot.motion.right.setSpeed(2500);
   } else {
     displayMessage("GRÜN >= ROT: Links drehen.");
-    dezibot.motion.left.setSpeed(2500); // Nach links drehen
+    dezibot.motion.left.setSpeed(2500);
     dezibot.motion.right.setSpeed(BASE_SPEED);
   }
+
+  //TODO: Einbauen das es bei Sackgasse flipped und wieder auf die Linie zurückdreht
+  //TODO: Methoden links abbiegen, rechts abbiegen, gerade evtl?
+  //TODO: Flip Logik selbst
 }
 
 void getColorPercentages(double &percentageRed, double &percentageGreen, double &percentageBlue) {
-  uint16_t red = dezibot.colorDetection.getColorValue(VEML_RED);
+  uint16_t red = dezibot.colorDetection.getColorValue(VEML_RED) * 1.05;
   uint16_t green = dezibot.colorDetection.getColorValue(VEML_GREEN);
   uint16_t blue = dezibot.colorDetection.getColorValue(VEML_BLUE);
   double sumColor = red + green + blue;
