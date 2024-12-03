ae86993f426caeee68092eec7e818f4e73d0b5e2
Vladislav Antonov <vladislav.antonov@stud.htwk-leipzig.de>
2024-12-03 14:54:50 +0100

    Verbessertes Movement

--- example/LabyrinthSolver/MoveTest.ino
+++ example/LabyrinthSolver/MoveTest.ino
@@ -4,45 +4,33 @@ Dezibot dezibot = Dezibot();
 
 const double BASE_SPEED = 3900;
 const double MAX_SPEED = 8192;
+const double ROTATE_SPEED = 2500;
+const double RED_SCALE = 1.0075;
+
+bool invertComparison = false;
 
 void setup() {
   Serial.begin(115200);
   dezibot.begin();
   dezibot.multiColorLight.setLed(BOTTOM, 100, 100, 100);
 }
-
+//TODO: Scale Wert für dunklen Raum --> mit perfekten Hütchen messen
 void loop() {
   double percentageRed, percentageGreen, percentageBlue;
   getColorPercentages(percentageRed, percentageGreen, percentageBlue);
 
-  dezibot.display.clear();
-  dezibot.display.println("R:" + String(percentageRed, 2) + " G:" + String(percentageGreen, 2));
-  delay(100);
+  bool isFirstGreater = compareColors(percentageRed, percentageGreen);
+  controlMotors(isFirstGreater);
+}
 
-  // while(true){
-  // dezibot.motion.right.setSpeed(8192);
-  // if(percentageGreen-percentageRed >= 1){
+/////////////////////////////////////////////////////////////////////////////////////
 
-  // }
-  // }
-
-  if (percentageRed > percentageGreen) {
-    displayMessage("ROT > GRÜN: Rechts drehen.");
-    dezibot.motion.left.setSpeed(BASE_SPEED);
-    dezibot.motion.right.setSpeed(2500);
-  } else {
-    displayMessage("GRÜN >= ROT: Links drehen.");
-    dezibot.motion.left.setSpeed(2500);
-    dezibot.motion.right.setSpeed(BASE_SPEED);
-  }
-
-  //TODO: Einbauen das es bei Sackgasse flipped und wieder auf die Linie zurückdreht
-  //TODO: Methoden links abbiegen, rechts abbiegen, gerade evtl?
-  //TODO: Flip Logik selbst
+void colorSwitch() {
+  invertComparison = !invertComparison;
 }
 
 void getColorPercentages(double &percentageRed, double &percentageGreen, double &percentageBlue) {
-  uint16_t red = dezibot.colorDetection.getColorValue(VEML_RED) * 1.05;
+  uint16_t red = dezibot.colorDetection.getColorValue(VEML_RED) * RED_SCALE;
   uint16_t green = dezibot.colorDetection.getColorValue(VEML_GREEN);
   uint16_t blue = dezibot.colorDetection.getColorValue(VEML_BLUE);
   double sumColor = red + green + blue;
@@ -50,6 +38,20 @@ void getColorPercentages(double &percentageRed, double &percentageGreen, double 
   percentageRed = (red / sumColor) * 100.0;
   percentageGreen = (green / sumColor) * 100.0;
   percentageBlue = (blue / sumColor) * 100.0;
+}
+
+bool compareColors(double percentageRed, double percentageGreen) {
+  return invertComparison ? (percentageGreen > percentageRed) : (percentageRed > percentageGreen);
+}
+
+void controlMotors(bool isFirstGreater) {
+  if (isFirstGreater) {
+    dezibot.motion.left.setSpeed(BASE_SPEED);
+    dezibot.motion.right.setSpeed(ROTATE_SPEED);
+  } else {
+    dezibot.motion.left.setSpeed(ROTATE_SPEED);
+    dezibot.motion.right.setSpeed(BASE_SPEED);
+  }
 }
 
 void displayMessage(const char *message) {
