4113c9dee27362ade2179ef21da046cb1af2cc95
Vladislav Antonov <vladislav.antonov@stud.htwk-leipzig.de>
2024-12-26 15:46:55 +0100

    Kalibrierung aktiviert

--- example/LabyrinthSolver/LabyrinthSolver.ino
+++ example/LabyrinthSolver/LabyrinthSolver.ino
@@ -8,7 +8,7 @@ void setup() {
     dezibot.begin();
     dezibot.multiColorLight.setLed(BOTTOM, 100, 100, 100);
     dezibot.labyrinthMovement.setColorMode(GREEN_LEFT);
-    //dezibot.labyrinthMovement.calibrateWhite();
+    dezibot.labyrinthMovement.calibrateWhite();
 }
 
 void loop() {
@@ -22,14 +22,14 @@ void loop() {
     // dezibot.display.clear();
     // //DEBUG_COLOR_CALIBRATION_END
 
-    // dezibot.labyrinthMovement.setIsOnWhite(
-    //     dezibot.labyrinthMovement.isColorCloseTo(dezibot.labyrinthMovement.getCalibratedRed(), percentageRed, config.getWhiteTolerance()) &&
-    //     dezibot.labyrinthMovement.isColorCloseTo(dezibot.labyrinthMovement.getCalibratedGreen(), percentageGreen, config.getWhiteTolerance())
-    // );
+    dezibot.labyrinthMovement.setIsOnWhite(
+        dezibot.labyrinthMovement.isColorCloseTo(dezibot.labyrinthMovement.getCalibratedRed(), percentageRed, config.getWhiteTolerance()) &&
+        dezibot.labyrinthMovement.isColorCloseTo(dezibot.labyrinthMovement.getCalibratedGreen(), percentageGreen, config.getWhiteTolerance())
+    );
 
-    // if (dezibot.labyrinthMovement.getIsOnWhite()) {
-    //     dezibot.labyrinthMovement.deadEndRotation();
-    // }
+    if (dezibot.labyrinthMovement.getIsOnWhite()) {
+        dezibot.labyrinthMovement.deadEndRotation();
+    }
 
     bool isFirstGreater = dezibot.labyrinthMovement.compareColors(percentageRed, percentageGreen);
     dezibot.labyrinthMovement.controlMotors(isFirstGreater);
