3eec0c331c4ca46e462490b3d609352ebc6ef3a4
Vladislav Antonov <vladislav.antonov@stud.htwk-leipzig.de>
2024-12-29 13:30:39 +0100

    Docs hinzugefügt

--- src/labyrinthSolver/LabyrinthConfig.h
+++ src/labyrinthSolver/LabyrinthConfig.h
@@ -1,3 +1,14 @@
+/**
+ * @file LabyrinthConfig.h
+ * @brief Header file for the LabyrinthConfig class.
+ * @version 0.1
+ * @date 2023-10-10
+ * 
+ * @author Vladislav Antonov
+ * 
+ * This file contains the declaration of the LabyrinthConfig class, which holds configuration settings for the labyrinth solver.
+ */
+
 #ifndef LABYRINTH_CONFIG_H
 #define LABYRINTH_CONFIG_H
 
@@ -5,22 +16,100 @@
 
 class LabyrinthConfig {
 public:
+    /**
+     * @brief Gets the base speed of the robot.
+     * @return uint16_t The base speed.
+     */
     uint16_t getBaseSpeed() const;
+
+    /**
+     * @brief Gets the maximum speed of the robot.
+     * @return uint16_t The maximum speed.
+     */
     uint16_t getMaxSpeed() const;
+
+    /**
+     * @brief Gets the rotation speed of the robot.
+     * @return uint16_t The rotation speed.
+     */
     uint16_t getRotateSpeed() const;
+
+    /**
+     * @brief Gets the red color scale factor.
+     * @return double The red color scale factor.
+     */
     double getRedScale() const;
+
+    /**
+     * @brief Gets the green color scale factor.
+     * @return double The green color scale factor.
+     */
     double getGreenScale() const;
+
+    /**
+     * @brief Gets the rotation duration.
+     * @return int The rotation duration in milliseconds.
+     */
     int getRotateDuration() const;
+
+    /**
+     * @brief Gets the duration for moving straight.
+     * @return int The duration for moving straight in milliseconds.
+     */
     int getMoveStraightDuration() const;
+
+    /**
+     * @brief Gets the tolerance for detecting white color.
+     * @return double The white color tolerance.
+     */
     double getWhiteTolerance() const;
 
+    /**
+     * @brief Sets the base speed of the robot.
+     * @param speed The base speed.
+     */
     void setBaseSpeed(uint16_t speed);
+
+    /**
+     * @brief Sets the maximum speed of the robot.
+     * @param speed The maximum speed.
+     */
     void setMaxSpeed(uint16_t speed);
+
+    /**
+     * @brief Sets the rotation speed of the robot.
+     * @param speed The rotation speed.
+     */
     void setRotateSpeed(uint16_t speed);
+
+    /**
+     * @brief Sets the red color scale factor.
+     * @param scale The red color scale factor.
+     */
     void setRedScale(double scale);
+
+    /**
+     * @brief Sets the green color scale factor.
+     * @param scale The green color scale factor.
+     */
     void setGreenScale(double scale);
+
+    /**
+     * @brief Sets the rotation duration.
+     * @param duration The rotation duration in milliseconds.
+     */
     void setRotateDuration(int duration);
+
+    /**
+     * @brief Sets the duration for moving straight.
+     * @param duration The duration for moving straight in milliseconds.
+     */
     void setMoveStraightDuration(int duration);
+
+    /**
+     * @brief Sets the tolerance for detecting white color.
+     * @param tolerance The white color tolerance.
+     */
     void setWhiteTolerance(double tolerance);
 
 private:
--- src/labyrinthSolver/LabyrinthMovement.h
+++ src/labyrinthSolver/LabyrinthMovement.h
@@ -1,3 +1,14 @@
+/**
+ * @file LabyrinthMovement.h
+ * @brief Header file for the LabyrinthMovement class.
+ * @version 0.1
+ * @date 2023-10-10
+ * 
+ * @author Vladislav Antonov
+ * 
+ * This file contains the declaration of the LabyrinthMovement class, which handles the movement logic for the labyrinth solver.
+ */
+
 #ifndef LABYRINTH_MOVEMENT_H
 #define LABYRINTH_MOVEMENT_H
 
@@ -9,27 +20,149 @@
 
 class LabyrinthMovement {
 public:
+    /**
+     * @brief Constructs a new LabyrinthMovement object.
+     * 
+     * @param config Reference to the LabyrinthConfig object.
+     */
     LabyrinthMovement(LabyrinthConfig& config);
 
+    /**
+     * @brief Sets the color mode.
+     * 
+     * @param colorMode The color mode to set.
+     */
     void setColorMode(int colorMode);
+
+    /**
+     * @brief Gets the current color mode.
+     * 
+     * @return int The current color mode.
+     */
     int getColorMode() const;
+
+    /**
+     * @brief Toggles the color mode between RED_LEFT and GREEN_LEFT.
+     */
     void toggleColorMode();
+
+    /**
+     * @brief Calibrates the white color.
+     */
     void calibrateWhite();
+
+    /**
+     * @brief Performs a dead-end rotation.
+     */
     void deadEndRotation();
+
+    /**
+     * @brief Moves the motor with specified speeds and duration.
+     * 
+     * @param leftSpeed The speed of the left motor.
+     * @param rightSpeed The speed of the right motor.
+     * @param duration The duration to move the motor.
+     */
     void moveMotor(uint16_t leftSpeed, uint16_t rightSpeed, int duration);
+
+    /**
+     * @brief Moves the robot straight.
+     */
     void moveStraight();
+
+    /**
+     * @brief Moves the robot to the left.
+     */
     void moveLeft();
+
+    /**
+     * @brief Moves the robot to the right.
+     */
     void moveRight();
+
+    /**
+     * @brief Stops the motors.
+     */
     void stopMotors();
+
+    /**
+     * @brief Switches the color comparison mode.
+     */
     void colorSwitch();
+
+    /**
+     * @brief Gets the color percentages.
+     * 
+     * @param percentageRed Reference to store the red percentage.
+     * @param percentageGreen Reference to store the green percentage.
+     * @param percentageBlue Reference to store the blue percentage.
+     */
     void getColorPercentages(double &percentageRed, double &percentageGreen, double &percentageBlue) const;
+
+    /**
+     * @brief Compares the red and green color percentages.
+     * 
+     * @param percentageRed The red color percentage.
+     * @param percentageGreen The green color percentage.
+     * @return true If the first color is greater.
+     * @return false Otherwise.
+     */
     bool compareColors(double percentageRed, double percentageGreen) const;
+
+    /**
+     * @brief Checks if the new color value is close to the initial value within a tolerance.
+     * 
+     * @param initialValue The initial color value.
+     * @param newValue The new color value.
+     * @param tolerance The tolerance value.
+     * @return true If the new value is close to the initial value.
+     * @return false Otherwise.
+     */
     bool isColorCloseTo(double initialValue, double newValue, double tolerance) const;
+
+    /**
+     * @brief Controls the motors based on the color comparison result.
+     * 
+     * @param isFirstGreater The result of the color comparison.
+     */
     void controlMotors(bool isFirstGreater);
+
+    /**
+     * @brief Sets the motor speeds.
+     * 
+     * @param leftSpeed The speed of the left motor.
+     * @param rightSpeed The speed of the right motor.
+     */
     void setMotorSpeeds(uint16_t leftSpeed, uint16_t rightSpeed);
 
+    /**
+     * @brief Gets the state of whether the robot is on white.
+     * 
+     * @return true If the robot is on white.
+     * @return false Otherwise.
+     */
     bool getIsOnWhite() const;
+
+    /**
+     * @brief Sets the state of whether the robot is on white.
+     * 
+     * @param value The state to set.
+     */
     void setIsOnWhite(bool value);
+
+    /**
+     * @brief Gets the calibrated red color value.
+     * 
+     * @return double The calibrated red color value.
+     */
+    double getCalibratedRed() const;
+
+    /**
+     * @brief Gets the calibrated green color value.
+     * 
+     * @return double The calibrated green color value.
+     */
+    double getCalibratedGreen() const;
 
 private:
     LabyrinthConfig& config;
