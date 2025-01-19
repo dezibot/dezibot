fa52d23ccdd7397aad628303d2b728ba63b71a7d
Max Mager <max.mager@stud.htwk-leipzig.de>
2025-01-20 00:33:44 +0100

    adjust path

--- example/LabyrinthSolver/LabyrinthSolver/CrossingModelT.h
+++ example/LabyrinthSolver/LabyrinthSolver/CrossingModelT.h
@@ -7,7 +7,7 @@
 #include "tensorflow/lite/micro/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/schema/schema_generated.h"
-#include "/CommonDefinitions.h"
+#include "CommonDefinitions.h"
 
 
 class CrossingPredictorT {
