664a7e99e7d46ac2e0d83cb43a2117ea1beb5192
FelixHerrling <felixherrling.business@gmail.com>
2025-01-30 11:44:43 +0100

    implemented example page for live data to test functionality

--- src/debugServer/LiveDataPage.cpp
+++ src/debugServer/LiveDataPage.cpp
@@ -1,8 +1,20 @@
 #include "LiveDataPage.h"
+#include <Dezibot.h>
 
-LiveDataPage::LiveDataPage(WebServer* server):serverPointer(server) {}
+extern Dezibot dezibot;
+
+LiveDataPage::LiveDataPage(WebServer* server):serverPointer(server)
+{
+    // Setup separate endpoint for AJAX requests
+    server->on("/lightDetection.getValue", [this]() {
+        String lightValueStr = String(dezibot.lightDetection.getValue(DL_FRONT));
+        String jsonResponse = "{\"value\": " + lightValueStr + "}";
+        serverPointer->send(200, "application/json", jsonResponse); // Send JSON response
+    });
+}
 
 void LiveDataPage::handler() {
-    String s = "<html><body><h1>Live Data</h1></body></html>";
-    serverPointer->send(200, "text/html", s);
+    String htmlContent = readHtmlFromFile("/LiveDataPage.html");
+    serverPointer->send(200, "text/html", htmlContent);
+
 };
