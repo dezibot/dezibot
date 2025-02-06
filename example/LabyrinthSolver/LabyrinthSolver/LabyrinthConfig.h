862da38e11602f5295db2f6498fb33bca370b127
Vladislav Antonov <vladislav.antonov@stud.htwk-leipzig.de>
2025-02-06 22:16:15 +0100

    MaxMager Angabe und alte Dateien entfernt

--- example/LabyrinthSolver/LabyrinthSolver/LabyrinthConfig.h
+++ example/LabyrinthSolver/LabyrinthSolver/LabyrinthConfig.h
@@ -4,7 +4,7 @@
  * @version 0.1
  * @date 2024-12-30
  * 
- * @author Vladislav Antonov
+ * @author Vladislav Antonov, Max Mager
  * 
  * This file contains the declaration of the LabyrinthConfig class, which holds configuration settings for the labyrinth solver.
  */
