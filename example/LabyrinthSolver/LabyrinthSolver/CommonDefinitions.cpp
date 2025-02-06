b4f35b6fb9edb798928545b25cbe17b4a542d9bb
Vladislav Antonov <vladislav.antonov@stud.htwk-leipzig.de>
2025-02-06 22:20:21 +0100

    trigger typo

--- example/LabyrinthSolver/LabyrinthSolver/CommonDefinitions.cpp
+++ example/LabyrinthSolver/LabyrinthSolver/CommonDefinitions.cpp
@@ -4,12 +4,12 @@ VEML_CONFIG ManualConfig80 = {
     .mode = MANUAL,
     .enabled = true,
     .exposureTime = MS80, 
-    .triggerEnabeled = true
+    .triggerEnabled = true
 };
 
 VEML_CONFIG ManualConfig320 = {
     .mode = MANUAL,
     .enabled = true,
     .exposureTime = MS320, 
-    .triggerEnabeled = true
+    .triggerEnabled = true
 };
