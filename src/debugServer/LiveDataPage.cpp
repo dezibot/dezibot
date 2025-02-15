a0dacfab26ac08380b8fccc314a555f1baa082bd
FelixHerrling <felixherrling.business@gmail.com>
2025-02-15 17:24:23 +0100

    Implemented basic functionality of LiveDataPage

--- src/debugServer/LiveDataPage.cpp
+++ src/debugServer/LiveDataPage.cpp
@@ -1,19 +1,55 @@
 #include "LiveDataPage.h"
 #include <Dezibot.h>
+#include <ArduinoJson.h>
 
 extern Dezibot dezibot;
 
 LiveDataPage::LiveDataPage(WebServer* server): serverPointer(server)
 {
-    server->on("/lightDetection.getValue", [this]()
-    {
-        String lightValueStr = String(dezibot.lightDetection.getValue(DL_FRONT));
-        String jsonResponse = "{\"value\": " + lightValueStr + "}";
-        serverPointer->send(200, "application/json", jsonResponse);
+    server->on("/getEnabledSensorValues", [this]() {
+        getEnabledSensorValues();
     });
+
+    // add functions to map for faster access
+    sensorValueFunctions["cd_getAmbientLight"] = [this](JsonObject& sensorObject) {
+        sensorObject["value"] = dezibot.colorDetection.getAmbientLight();
+    };
+
+    sensorValueFunctions["ld_getValueDlbottom"] = [this](JsonObject& sensorObject) {
+        sensorObject["value"] = dezibot.lightDetection.getValue(DL_BOTTOM);
+    };
+    // TODO: add all sensor functions
 }
 
 void LiveDataPage::handler() {
     String htmlContent = readHtmlFromFile("/LiveDataPage.html");
     serverPointer->send(200, "text/html", htmlContent);
 };
+
+void LiveDataPage::getEnabledSensorValues()
+{
+    DynamicJsonDocument jsonDoc(1024);
+    JsonArray sensorArray = jsonDoc.to<JsonArray>();
+    auto& sensorStates = dezibot.debugServer.getSensorStates();
+
+    for (const auto& [sensor, status] : sensorStates) {
+        if (status) {
+            if (sensorValueFunctions.find(sensor) != sensorValueFunctions.end()) {
+                JsonObject sensorObject = sensorArray.createNestedObject();
+
+                // format string to match function name
+                int underscoreIndex = sensor.indexOf("_");
+                String name = sensor.substring(underscoreIndex + 1);
+                name += "()";
+                sensorObject["name"] = name;
+
+                // call function to get value
+                sensorValueFunctions[sensor](sensorObject);
+            }
+        }
+    }
+
+    String jsonResponse;
+    serializeJson(jsonDoc, jsonResponse);
+    serverPointer->send(200, "application/json", jsonResponse);
+}
--- src/debugServer/LiveDataPage.h
+++ src/debugServer/LiveDataPage.h
@@ -1,17 +1,18 @@
 #ifndef LIVEDATAPAGE_H
 #define LIVEDATAPAGE_H
 
+#include <ArduinoJson.h>
 #include "PageProvider.h"
-#include "SettingsPage.h"
+#include <map>
 
 class LiveDataPage: public PageProvider {
 private:
     WebServer* serverPointer;
+    std::map<String, std::function<void(JsonObject&)>> sensorValueFunctions;
 public:
     LiveDataPage(WebServer* server);
     void handler() override;
+    void getEnabledSensorValues();
 };
-
-
 
 #endif //LIVEDATAPAGE_H
