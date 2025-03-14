b6ef36bd2c431152398322f06a48cefb3970bfbe
FelixHerrling <felixherrling.business@gmail.com>
2025-03-14 13:45:08 +0100

    refactoring and documentation

--- src/debugServer/DebugServer.cpp
+++ src/debugServer/DebugServer.cpp
@@ -13,38 +13,18 @@ extern Dezibot dezibot;
 
 DebugServer::DebugServer():server(80)
 {
-    mainPage = nullptr;
-    loggingPage = nullptr;
-    liveDataPage = nullptr;
-    settingsPage = nullptr;
-    // TODO: Maybe move this to SettingsPage or create separate class for sensor settings
-    sensorStates["cd_getAmbientLight"] = true;
-    sensorStates["cd_getColorValueRed"] = true;
-    sensorStates["cd_getColorValueGreen"] = true;
-    sensorStates["cd_getColorValueBlue"] = true;
-    sensorStates["cd_getColorValueWhite"] = true;
-    sensorStates["ld_getValueIrfront"] = true;
-    sensorStates["ld_getValueIrleft"] = true;
-    sensorStates["ld_getValueIrright"] = true;
-    sensorStates["ld_getValueIrback"] = true;
-    sensorStates["ld_getValueDlbottom"] = true;
-    sensorStates["ld_getValueDlfront"] = true;
-    sensorStates["ld_getBrightestIr"] = true;
-    sensorStates["ld_getBrightestDl"] = true;
-    sensorStates["m_getSpeedLeft"] = true;
-    sensorStates["m_getSpeedRight"] = true;
-    sensorStates["md_getAcceleration"] = true;
-    sensorStates["md_getRotation"] = true;
-    sensorStates["md_getTemperature"] = true;
-    sensorStates["md_getWhoAmI"] = true;
-    sensorStates["md_getTilt"] = true;
-    sensorStates["md_getTiltDirection"] = true;
+    mainPage = new MainPage(&server);
+    loggingPage = new LoggingPage(&server);
+    settingsPage = new SettingsPage(&server);
+    liveDataPage = new LiveDataPage(&server);
 }
 
 void DebugServer::setup() {
+    // set wifi credentials
     const char* SSID = "Debug-Server";
     const char* PSK = "PW4studProj";
 
+    // set IP configuration
     const IPAddress local_ip(192,168,1,1);
     const IPAddress gateway(192,168,1,1);
     const IPAddress subnet(255,255,255,0);
@@ -56,12 +36,6 @@ void DebugServer::setup() {
     // setup as wifi accesspoint
     WiFi.softAP(SSID, PSK);
     WiFi.softAPConfig(local_ip, gateway, subnet);
-
-    // Initiate page objects
-    mainPage = new MainPage(&server);
-    loggingPage = new LoggingPage(&server);
-    settingsPage = new SettingsPage(&server);
-    liveDataPage = new LiveDataPage(&server);
 
     // set uri and handler for each page
     server.on("/", [this]() {
@@ -82,11 +56,11 @@ void DebugServer::setup() {
 
     // initialize color sensor
     Sensor colorSensor("Color Sensor", "ColorDetection");
-    SensorFunction getAmbientLight("getAmbientLight()", [&]() { dezibot.colorDetection.getAmbientLight(); });
-    SensorFunction getColorValueRed("getColorValue(RED)", [&]() { dezibot.colorDetection.getColorValue(VEML_RED); });
-    SensorFunction getColorValueGreen("getColorValue(GREEN)", [&]() { dezibot.colorDetection.getColorValue(VEML_GREEN); });
-    SensorFunction getColorValueBlue("getColorValue(BLUE)", [&]() { dezibot.colorDetection.getColorValue(VEML_BLUE); });
-    SensorFunction getColorValueWhite("getColorValue(WHITE)", [&]() { dezibot.colorDetection.getColorValue(VEML_WHITE); });
+    SensorFunction getAmbientLight("getAmbientLight()");
+    SensorFunction getColorValueRed("getColorValue(RED)");
+    SensorFunction getColorValueGreen("getColorValue(GREEN)");
+    SensorFunction getColorValueBlue("getColorValue(BLUE)") ;
+    SensorFunction getColorValueWhite("getColorValue(WHITE)");
     colorSensor.addFunction(getAmbientLight);
     colorSensor.addFunction(getColorValueRed);
     colorSensor.addFunction(getColorValueGreen);
@@ -96,12 +70,12 @@ void DebugServer::setup() {
 
     // initialize light sensor
     Sensor lightSensor("Light Sensor", "LightDetection");
-    SensorFunction getValueIrFront("getValue(IR_FRONT)", [&]() { LightDetection::getValue(IR_FRONT); });
-    SensorFunction getValueIrLeft("getValue(IR_LEFT)", [&]() { LightDetection::getValue(IR_LEFT); });
-    SensorFunction getValueIrRight("getValue(IR_RIGHT)", [&]() { LightDetection::getValue(IR_RIGHT); });
-    SensorFunction getValueIrBack("getValue(IR_BACK)", [&]() { LightDetection::getValue(IR_BACK); });
-    SensorFunction getValueDlBottom("getValue(DL_BOTTOM)", [&]() { LightDetection::getValue(DL_BOTTOM); });
-    SensorFunction getValueDlFront("getValue(DL_FRONT)", [&]() { LightDetection::getValue(DL_FRONT); });
+    SensorFunction getValueIrFront("getValue(IR_FRONT)");
+    SensorFunction getValueIrLeft("getValue(IR_LEFT)");
+    SensorFunction getValueIrRight("getValue(IR_RIGHT)");
+    SensorFunction getValueIrBack("getValue(IR_BACK)");
+    SensorFunction getValueDlBottom("getValue(DL_BOTTOM)");
+    SensorFunction getValueDlFront("getValue(DL_FRONT)");
     lightSensor.addFunction(getValueIrFront);
     lightSensor.addFunction(getValueIrLeft);
     lightSensor.addFunction(getValueIrRight);
@@ -112,20 +86,20 @@ void DebugServer::setup() {
 
     // initialize motor
     Sensor motor("Motor", "Motion");
-    SensorFunction getSpeedLeft("left.getSpeed()", [&]() { Motion::left.getSpeed(); });
-    SensorFunction getSpeedRight("right.getSpeed()", [&]() { Motion::left.getSpeed(); });
+    SensorFunction getSpeedLeft("left.getSpeed()");
+    SensorFunction getSpeedRight("right.getSpeed()");
     motor.addFunction(getSpeedLeft);
     motor.addFunction(getSpeedRight);
     addSensor(motor);
 
     // initialize motion sensor
     Sensor motionSensor("Motion Sensor", "MotionDetection");
-    SensorFunction getAcceleration("getAcceleration()", [&]() { Motion::detection.getAcceleration(); });
-    SensorFunction getRotation("getRotation()", [&]() { Motion::detection.getRotation(); });
-    SensorFunction getTemperature("getTemperature()", [&]() { Motion::detection.getTemperature(); });
-    SensorFunction getWhoAmI("getWhoAmI()", [&]() { Motion::detection.getWhoAmI(); });
-    SensorFunction getTilt("getTilt()", [&]() { Motion::detection.getTilt(); });
-    SensorFunction getTiltDirection("getTiltDirection()", [&]() { Motion::detection.getTiltDirection(); });
+    SensorFunction getAcceleration("getAcceleration()");
+    SensorFunction getRotation("getRotation()");
+    SensorFunction getTemperature("getTemperature()");
+    SensorFunction getWhoAmI("getWhoAmI()");
+    SensorFunction getTilt("getTilt()");
+    SensorFunction getTiltDirection("getTiltDirection()");
     motionSensor.addFunction(getAcceleration);
     motionSensor.addFunction(getRotation);
     motionSensor.addFunction(getTemperature);
@@ -134,33 +108,23 @@ void DebugServer::setup() {
     motionSensor.addFunction(getTiltDirection);
     addSensor(motionSensor);
 
+    // start webserver and run client handler on separate thread
     server.begin();
     beginClientHandle();
 };
 
-bool DebugServer::getSensorState(const String& sensor) {
-    return sensorStates[sensor];
-}
-
-void DebugServer::setSensorState(const String& sensor, bool state) {
-    sensorStates[sensor] = state;
-}
-
-std::map<String, bool>& DebugServer::getSensorStates() {
-    return sensorStates;
-}
-
+// add sensor to the list of sensors
 void DebugServer::addSensor(const Sensor& sensor) {
     sensors.push_back(sensor);
 }
 
+// get all sensors
 std::vector<Sensor>& DebugServer::getSensors() {
     return sensors;
 }
 
-
+// create a FreeRTOS task to handle client requests
 void DebugServer::beginClientHandle() {
-    // create a FreeRTOS task to handle client requests
     xTaskCreate(
         handleClientTask,
         "DebugServerTask",
@@ -171,8 +135,9 @@ void DebugServer::beginClientHandle() {
     );
 }
 
+// handle client requests
 void DebugServer::handleClientTask(void* parameter) {
-    DebugServer* debugServer = static_cast<DebugServer*>(parameter);
+     auto debugServer = static_cast<DebugServer*>(parameter);
 
     // continuously handle client requests
     while (debugServer->serveractive) {
--- src/debugServer/DebugServer.h
+++ src/debugServer/DebugServer.h
@@ -5,9 +5,6 @@
 #include "LiveDataPage.h"
 #include "SettingsPage.h"
 #include "Sensor.h"
-#include "SensorFunction.h"
-#include <map>
-#include <thread>
 
 class DebugServer {
 private:
@@ -16,17 +13,12 @@ private:
     LoggingPage* loggingPage;
     LiveDataPage* liveDataPage;
     SettingsPage* settingsPage;
-    std::thread handleClient();
-    std::map<String, bool> sensorStates; // contains state (enabled/disabled) for each sensor
     bool serveractive = true;
     static void handleClientTask(void* parameter);
     std::vector<Sensor> sensors;
 public:
     DebugServer();
     void setup();
-    bool getSensorState(const String& sensor);
-    void setSensorState(const String& sensor, bool state);
-    std::map<String, bool>& getSensorStates();
     void beginClientHandle();
     void addSensor(const Sensor& sensor);
     std::vector<Sensor>& getSensors();
