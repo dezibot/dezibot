f9ddf673a303f5d94890d50086ad1efd18f8b1fe
FelixHerrling <felixherrling.business@gmail.com>
2025-03-14 14:02:36 +0100

    refactoring and documentation

--- src/debugServer/Utility.cpp
+++ src/debugServer/Utility.cpp
@@ -1,30 +1,7 @@
 #include <Dezibot.h>
 #include "Utility.h"
 
-std::map<String, String> Utility::sensorFunctionNames = {
-    {"cd_getAmbientLight", "ColorDetection.getAmbientLight()"},
-    {"cd_getColorValueRed", "ColorDetection.getColorValue(VEML_RED)"},
-    {"cd_getColorValueGreen", "ColorDetection.getColorValue(VEML_GREEN)"},
-    {"cd_getColorValueBlue", "ColorDetection.getColorValue(VEML_BLUE)"},
-    {"cd_getColorValueWhite", "ColorDetection.getColorValue(VEML_WHITE)"},
-    {"ld_getValueIrfront", "LightDetection.getValue(IR_FRONT)"},
-    {"ld_getValueIrleft", "LightDetection.getValue(IR_LEFT)"},
-    {"ld_getValueIrright", "LightDetection.getValue(IR_RIGHT)"},
-    {"ld_getValueIrback", "LightDetection.getValue(IR_BACK)"},
-    {"ld_getValueDlbottom", "LightDetection.getValue(DL_BOTTOM)"},
-    {"ld_getValueDlfront", "LightDetection.getValue(DL_FRONT)"},
-    {"ld_getBrightestIr", "ightDetection.getBrightest(IR)"},
-    {"ld_getBrightestDl", "LightDetection.getBrightest(DAYLIGHT)"},
-    {"m_getSpeedLeft", "Motion.left.getSpeed()"},
-    {"m_getSpeedRight", "Motion.right.getSpeed()"},
-    {"md_getAcceleration", "Motion.detection.getAcceleration()"},
-    {"md_getRotation", "Motion.detection.getRotation()"},
-    {"md_getTemperature", "Motion.detection.getTemperature()"},
-    {"md_getWhoAmI", "Motion.detection.getWhoAmI()"},
-    {"md_getTilt", "Motion.detection.getTilt()"},
-    {"md_getTiltDirection", "Motion.detection.getTiltDirection()"}
-};
-
+// get string representation of the Direction enum
 String Utility::directionToString(Direction direction) {
     switch (direction) {
         case Front:
@@ -45,6 +22,7 @@ String Utility::directionToString(Direction direction) {
     return "Error";
 }
 
+// get string representation of the photoTransistors enum
 String Utility::sensorToString(photoTransistors sensor) {
     switch (sensor) {
         case IR_FRONT:
@@ -61,8 +39,4 @@ String Utility::sensorToString(photoTransistors sensor) {
             return "DL_FRONT";
     }
     return "Error";
-}
-
-String Utility::sensorToFunction(const String& sensor) {
-    return sensorFunctionNames[sensor];
 }
--- src/debugServer/Utility.h
+++ src/debugServer/Utility.h
@@ -4,12 +4,10 @@
 
 class Utility {
 private:
-    static std::map<String, String> sensorFunctionNames;
 public:
     Utility();
     static String directionToString(Direction direction);
     static String sensorToString(photoTransistors sensor);
-    static String sensorToFunction(const String& sensor);
 };
 
 #endif //UTILITY_H
